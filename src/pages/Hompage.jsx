56d7e247795cd9b5bef109a9de83b5481ec361b9
isiagi <isiagigeofrey0@gmail.com>
2022-05-20 09:28:33 +0300

    first commit

---
+++ src/pages/Hompage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/nav/Nav";
+
+const Hompage = () => {
+  return (
+    <div className="parent">
+      <div className="mxWidth">
+          <Nav />
+      </div>
+    </div>
+  );
+};
+
+export default Hompage;
---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hompage from './Hompage'
+
+const Index = () => {
+  return (
+    <div>
+        <Hompage />
+    </div>
+  )
+}
+
+export default Index
