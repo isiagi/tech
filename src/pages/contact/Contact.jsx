d6a65ac1349342f78cf6f5f7c963c04d369b2609
isiagi <isiagigeofrey0@gmail.com>
2022-06-08 13:34:52 +0300

    adds contact banner

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,9 @@
 import "./contact.css";
 import img from "../../assets/images/contact.png";
 import MapComponent from "./Map";
+import { GrMapLocation } from "react-icons/gr";
+import { AiOutlineMail } from "react-icons/ai";
+import { CgPhone } from "react-icons/cg";
 
 const Contact = () => {
   return (
@@ -17,26 +20,50 @@ const Contact = () => {
             </p>
           </div>
           <div className="contact__gird">
-            <div style={{width: '100%', height: '100%', backgroundColor: 'green'}}>
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: "green",
+              }}
+            >
               <img src={img} alt="" />
             </div>
             <div>
               <form>
                 <div className="form__row">
-                  <input type="text" placeholder="First Name"/>
-                  <input type="text" placeholder="Last Name"/>
+                  <input type="text" placeholder="First Name" />
+                  <input type="text" placeholder="Last Name" />
                 </div>
                 <div className="form__row">
-                  <input type="text" placeholder="Enter Email"/>
-                  <input type="text" placeholder="Enter Company"/>
+                  <input type="text" placeholder="Enter Email" />
+                  <input type="text" placeholder="Enter Company" />
                 </div>
                 <div className="form__row textarea">
-                    <textarea rows={9} placeholder="Enter Massage Here"/>
+                  <textarea rows={9} placeholder="Enter Massage Here" />
                 </div>
                 <div className="form__btn">
-                  <button type="submit" className="btn">Submit</button>
+                  <button type="submit" className="btn">
+                    Submit
+                  </button>
                 </div>
               </form>
+            </div>
+          </div>
+          <div className="contact__info">
+            <div>
+              <CgPhone className="icon" />
+              <p>+256 785988864</p>
+              <p>+256 758914876</p>
+            </div>
+            <div>
+              <GrMapLocation className="icon" />
+              <p>Ham Towers,Level 3, RM HT28 Opp Makerere Main Gate, Kampala</p>
+            </div>
+            <div>
+              <AiOutlineMail className="icon" />
+              <p>manisa@happitechcorporation.com</p>
+              <p>info@happitechcorporation.com</p>
             </div>
           </div>
           <div className="contact__map">
