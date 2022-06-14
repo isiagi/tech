eb36ba4643954bdcd031df5843dd49fd0f72bf50
isiagi <isiagigeofrey0@gmail.com>
2022-06-14 10:52:40 +0300

    adds system data

--- src/pages/system/System.jsx
+++ src/pages/system/System.jsx
@@ -1,31 +1,32 @@
-import img from "../../assets/images/sot1.jpg";
+import img from "../../assets/images/sales.jpg";
+import img1 from "../../assets/images/salex.jpg";
+import img2 from "../../assets/images/slaes.jpg";
 import "./system.css";
 
 const System = () => {
   return (
     <div className="parent">
       <div className="mxWidt bg">
-      <div className="soft__container"></div>
-      <div className="soft__section">
-        <h2>Computer system sales and maintenance</h2>
-        <div className="ye__grid">
-            <img src={img} alt="" className="soft__img" />
+        <div className="soft__container"></div>
+        <div className="soft__section">
+          <h2>Computer system sales and maintenance</h2>
+          <div className="ye__grid">
+            <div style={{ position: "relative", width: "81%" }}>
+              <div className="ye__photo__grid">
+                <img src={img} alt="" className="soft__im" />
+                <img src={img1} alt="" className="soft__imx" />
+              </div>
+              <div className="ye__photo__gri">
+                <div className="ye__photo__gri__span"></div>
+                <img src={img2} alt="" className="soft__imj" />
+              </div>
+            </div>
             <div>
-              <h1>
-                Are you wondering why you would want to invest in custom
-                software development?
-              </h1>
-              <p>
-                Scalability is one of the most important advantages of
-                customized software. By developing custom solutions,
-                organizations can modify or add to their capacity as they
-                expand. The software is designed with specific infrastructure,
-                branding, and implementation needs in mind.{" "}
-              </p>
+              <h1>Computer system sales maintenance</h1>
             </div>
           </div>
           <h4>Loading ... </h4>
-      </div>
+        </div>
       </div>
     </div>
   );
